7e130a659d30d221dd6cd77937ac3c2e10f0f9dc
Kai Rösken <69394132+fastrockstar@users.noreply.github.com>
2022-03-29 21:31:07 +0200

    Add files via upload

---
+++ submodules/rules.py
@@ -0,0 +1,36 @@
+rule_intro = """# In order to help to keep the git log clean and consize
+# please follow these rules:"""
+
+rule_limit = """
+# 2. Limit the subject line to 50 characters
+# 6. Wrap the body at 72 characters
+"""
+
+rule_50 = """
+WARNING:
+# 2. Limit the subject line to 50 characters 
+"""
+
+rule_period = """
+# 4. Do not end the subject line with a period
+"""
+
+rule_cap = """
+# 3. Capitalize the subject line
+"""
+
+rules = """
+# That was close. You almost spoiled the eternal git log!
+#
+# Please follow these rules:
+#
+# SEVEN RULES FOR A GREAT COMMIT MESSAGE
+# ======================================
+# 1. Separate subject from body with a blank line
+# 2. Limit the subject line to 50 characters
+# 3. Capitalize the subject line
+# 4. Do not end the subject line with a period
+# 5. Use the imperative mood in the subject line
+# 6. Wrap the body at 72 characters
+# 7. Use the body to explain what and why vs. how
+"""
